4d4fd564ea96514e0d85c368614cf8385f032eab
Andreas Tasch <andy.tasch@gmail.com>
2018-03-14 23:09:39 +0100

    Minor changes in examples.

--- example/index.php
+++ example/index.php
@@ -31,8 +31,51 @@ if (is_null($client)) {
 
 // As example we get the public key of the lnd node.
 // See directory "src/Lnrpc" for available classes.
-$request = new Lnrpc\GetInfoRequest();
+print('GetInfo:' . PHP_EOL);
+$getInfoRequest = new Lnrpc\GetInfoRequest();
 /** @var $reply Lnrpc\GetInfoResponse */
-list($reply, $status) = $client->GetInfo($request)->wait();
+list($reply, $status) = $client->GetInfo($getInfoRequest)->wait();
 print_r($reply->getIdentityPubkey());
-print("\n");
+print(PHP_EOL);
+
+print('WalletBalance:' . PHP_EOL);
+$walletBalanceRequest = new Lnrpc\WalletBalanceRequest();
+$walletBalanceRequest->setWitnessOnly(FALSE);
+/** @var $reply Lnrpc\WalletBalanceResponse */
+list($reply, $status) = $client->WalletBalance($walletBalanceRequest)->wait();
+print_r($reply->getBalance());
+print(PHP_EOL);
+
+print('ChannelBalance:' . PHP_EOL);
+/** @var $reply Lnrpc\ChannelBalanceResponse */
+list($reply, $status) = $client->ChannelBalance(new Lnrpc\ChannelBalanceRequest())->wait();
+print_r($reply->getBalance());
+print(PHP_EOL);
+
+print('GetTransactions:' . PHP_EOL);
+/** @var $reply Lnrpc\TransactionDetails */
+list($reply, $status) = $client->GetTransactions(new Lnrpc\GetTransactionsRequest())->wait();
+$counter = 0;
+foreach ($reply->getTransactions()->getIterator() as $key => $value) {
+  /** var $value Lnrpc\Transaction */
+  print_r($value->getAmount());
+  print("\n");
+  // Lets exit after 3 txs...
+  $counter++;
+  if ($counter >= 3) break;
+}
+print(PHP_EOL);
+
+print('ListPeers:' . PHP_EOL);
+/** @var $reply Lnrpc\ListPeersResponse */
+list($reply, $status) = $client->ListPeers(new Lnrpc\ListPeersRequest())->wait();
+$counter = 0;
+foreach ($reply->getPeers()->getIterator() as $key => $value) {
+  /** var $value Lnrpc\Peer */
+  print_r($value->getPubKey());
+  print("\n");
+  // Lets exit after 3 txs...
+  $counter++;
+  if ($counter >= 3) break;
+}
+print(PHP_EOL);
