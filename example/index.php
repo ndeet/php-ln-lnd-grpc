c7cf02920d9e771564d5cec0b57702de02bbc54b
Andreas Tasch <andy.tasch@gmail.com>
2018-09-14 22:55:40 +0200

    Commiting example/index.php with uncommitted changes from a few months ago x-), untested.

--- example/index.php
+++ example/index.php
@@ -3,27 +3,32 @@
 // Path to autoload.php created by composer.
 require dirname(__FILE__).'/../vendor/autoload.php';
 
+
+error_reporting(2);
+
 // LND node IP and Port (lnd option "rpcport").
 $lndIp   = '127.0.0.1:10001';
 
 // We need to set env variables to make ssl work:
 putenv('GRPC_SSL_CIPHER_SUITES=ECDHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES128-SHA256');
 
-// This should be the public SSL certificate of your LND.
-// We try to load the TLS cert from your users home directory.
-// MacOS: ~/Library/Application Support/Lnd/tls.cert
-// Linux: ~/.lnd/tls.cert
+// Get tls and macaroon paths.
+$tlsPath = '';
+$macaroonPath = '';
 $local_user = posix_getpwuid(posix_getuid());
+
 switch (PHP_OS) {
   case "Darwin":
-    $tls_path = $local_user['dir'] . '/Library/Application Support/Lnd/tls.cert';
+    $tlsPath = $local_user['dir'] . '/Library/Application Support/Lnd/tls.cert';
+    $macaroonPath = $local_user['dir'] . '/Library/Application Support/Lnd/macaroon.admin';
     break;
 
   case "Linux":
-    $tls_path = $local_user['dir'] . '/.lnd/tls.cert';
+    $tlsPath = $local_user['dir'] . '/.lnd/tls.cert';
+    $macaroonPath = $local_user['dir'] . '/.lnd/macaroon.admin';
 }
 
-if (! $sslCert = file_get_contents($tls_path)) {
+if (! $sslCert = file_get_contents($tlsPath)) {
   $certError = <<<EOT
     tls.cert not found in "example" directory. Make sure to copy it from your 
     LND config directory.
@@ -34,10 +39,58 @@ EOT;
   throw new Exception($certError);
 }
 
-// Create new client using SSL connection.
-$client = new Lnrpc\LightningClient($lndIp, [
+//// OVERWRITE MACAROON locally
+$macaroonPath = "/Users/ndeet/go/dev/alice/test_data/admin.macaroon";
+
+// Unlock the encrypted wallet.
+$unlockWallet = new Lnrpc\WalletUnlockerClient($lndIp, [
   'credentials' => Grpc\ChannelCredentials::createSsl($sslCert)
 ]);
+if (is_null($unlockWallet)) {
+  throw new Exception('Could not connect or authenticate to LND node (unlockWallet).');
+}
+$unlockWalletRequest = new Lnrpc\UnlockWalletRequest();
+$unlockWalletRequest->setWalletPassword("alicelight");
+/** @var $reply Lnrpc\UnlockWalletResponse */
+list($reply, $status) = $unlockWallet->UnlockWallet($unlockWalletRequest)->wait();
+var_dump($reply);
+
+
+
+$channel_credentials = Grpc\ChannelCredentials::createComposite(
+  Grpc\ChannelCredentials::createSsl($sslCert),
+  Grpc\CallCredentials::createFromPlugin('addMacaroon')
+);
+
+$opts = [
+  'credentials' => $channel_credentials
+];
+
+function addMacaroon($context) {
+  return [
+    'Grpc-Metadata-macaroon' => [file_get_contents("/Users/ndeet/go/dev/alice/test_data/admin.macaroon")]
+    ];
+}
+
+// Create new client using SSL connection.
+//$client = new Lnrpc\LightningClient($lndIp, $opts);
+/*
+// Create new client using SSL connection.
+$client = new Lnrpc\LightningClient($lndIp, [
+  'credentials' => Grpc\ChannelCredentials::createSsl($sslCert),
+  'Grpc-Metadata-macaroon' => bin2hex(file_get_contents($macaroonPath))
+]);
+*/
+try {
+  $client = new Lnrpc\LightningClient($lndIp, [
+    'credentials' => Grpc\ChannelCredentials::createSsl($sslCert),
+    'update_metadata' => addMacaroon('kasdkf')
+  ]);
+  var_dump($client);
+} catch (Exception $e) {
+  throw new Exception($e->getMessage());
+}
+
 
 if (is_null($client)) {
   throw new Exception('Could not connect or authenticate to LND node.');
@@ -46,11 +99,16 @@ if (is_null($client)) {
 // As example we get the public key of the lnd node.
 // See directory "src/Lnrpc" for available classes.
 print('GetInfo:' . PHP_EOL);
-$getInfoRequest = new Lnrpc\GetInfoRequest();
-/** @var $reply Lnrpc\GetInfoResponse */
-list($reply, $status) = $client->GetInfo($getInfoRequest)->wait();
-print_r($reply->getIdentityPubkey());
-print(PHP_EOL);
+try {
+  $getInfoRequest = new Lnrpc\GetInfoRequest();
+  /** @var $reply Lnrpc\GetInfoResponse */
+  list($reply, $status) = $client->GetInfo($getInfoRequest, addMacaroon('bla'))->wait();
+  print_r($reply->getIdentityPubkey());
+  print(PHP_EOL);
+} catch (Exception $e) {
+  print($e->getMessage());
+}
+
 
 print('WalletBalance:' . PHP_EOL);
 $walletBalanceRequest = new Lnrpc\WalletBalanceRequest();
